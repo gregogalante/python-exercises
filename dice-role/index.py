4b8c8bdb170698eb3cbc4360707b0fc3beff0322
Gregorio Galante <gregogalante@gmail.com>
2018-01-10 20:54:27 +0100

    added dice role example

---
+++ dice-role/index.py
@@ -0,0 +1,17 @@
+import random
+import matplotlib.pyplot as plt
+
+def random_variable_of_dice_roll():
+    return random.randint(1, 7)
+
+avgs = []
+num_trials = range(100, 10000, 10)
+for num_trial in num_trials:
+    trials = []
+    for trial in range(1, num_trial):
+        trials.append(random_variable_of_dice_roll())
+    avgs.append(sum(trials) / float(num_trial))
+
+plt.plot(num_trials, avgs)
+plt.xlabel('Number of trials')
+plt.ylabel('Average')
