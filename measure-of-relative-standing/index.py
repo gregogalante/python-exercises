fbcac1948ccc3bb1479ae8a5bc58fe9a77397a40
Gregorio Galante <gregogalante@gmail.com>
2018-01-19 22:11:21 +0100

    update

--- measure-of-relative-standing/index.py
+++ measure-of-relative-standing/index.py
@@ -1,8 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pylab as p
 
 people_friends = [1100, 800, 250, 1900, 1453, 200, 39, 789, 1200, 456, 349, 222, 1093]
-
-# ugly version:
 
 z_scores = []
 
@@ -17,3 +17,8 @@ for people_friend in people_friends:
     z_scores.append(z)
 
 print 'the list of relative standings for people_friends is:', z_scores
+
+y_pos = range(len(people_friends))
+plt.bar(y_pos, z_scores)
+
+p.show()
